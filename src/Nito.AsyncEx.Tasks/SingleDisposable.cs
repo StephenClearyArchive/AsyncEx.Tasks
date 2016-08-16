a72ae7eeb4a26d17746c261a2d889a5b51e5c28e
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-08-15 23:20:15 -0400

    Moved SingleDisposable to Nito.Disposables.

--- src/Nito.AsyncEx.Tasks/SingleDisposable.cs
+++ src/Nito.AsyncEx.Tasks/SingleDisposable.cs
@@ -7,37 +7,17 @@ namespace Nito.AsyncEx
     /// A base class for disposables that need exactly-once semantics in a threadsafe way.
     /// </summary>
     /// <typeparam name="T">The type of "context" for the derived disposable.</typeparam>
-    public abstract class SingleDisposable<T> : IDisposable
+    [Obsolete("Use Nito.Disposables.SingleDisposable instead.")]
+    public abstract class SingleDisposable<T> : Disposables.SingleDisposable<T>
         where T : class
     {
         /// <summary>
-        /// The context. This may be <c>null</c>.
-        /// </summary>
-        private T _context;
-
-        /// <summary>
         /// Creates a disposable for the specified context.
         /// </summary>
-        /// <param name="context">The context passed to <see cref="Dispose(T)"/>. If this is <c>null</c>, then <see cref="Dispose(T)"/> will never be called.</param>
+        /// <param name="context">The context passed to <see cref="Disposables.SingleDisposable{T}.Dispose(T)"/>. If this is <c>null</c>, then <see cref="Disposables.SingleDisposable{T}.Dispose(T)"/> will never be called.</param>
         protected SingleDisposable(T context)
+            : base(context)
         {
-            _context = context;
-        }
-
-        /// <summary>
-        /// The actul disposal method, called only once from <see cref="Dispose()"/>. If the context passed to the constructor of this instance is <c>null</c>, then this method is never called.
-        /// </summary>
-        /// <param name="context">The context for the disposal operation. This is never <c>null</c>.</param>
-        protected abstract void Dispose(T context);
-
-        /// <summary>
-        /// Disposes this instance.
-        /// </summary>
-        public void Dispose()
-        {
-            var context = Interlocked.Exchange(ref _context, null);
-            if (context != null)
-                Dispose(context);
         }
     }
 }
